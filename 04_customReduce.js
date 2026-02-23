99bfcd6587e3bdff8f4a3804493af3d0e320c96d
JUDE <judeani60@gmail.com>
2026-02-23 22:49:19 +0100

    chore: add comments

--- 04_customReduce.js
+++ 04_customReduce.js
@@ -2,15 +2,24 @@
 
 let arr = [4, 5, 6, 8, 9];
 
+// using normal .reduce()
+
 let result1 = arr.reduce((acc, cur) => acc + cur, 0)
 
 console.log(`Result of normal JS reduce - ${result1}`);
 
+
+
 Array.prototype.judeReduce = function(myCallback, initialValue) {
 
+    // set the initial value of 0 to be the first accumulated value
     let accumulatedValue = initialValue;
 
+
+    // iterates the accumulated value and each value of the array
     for(let i = 0; i < this.length; i++) {
+
+        // quick way to assign the returned value of the callback to be the new accumulated value
         accumulatedValue = myCallback(accumulatedValue, this[i]);
     }
 
@@ -18,6 +27,7 @@ Array.prototype.judeReduce = function(myCallback, initialValue) {
 }
 
 
+// using my custom .judeReduce()
 let result2 = arr.judeReduce((acc, cur) => acc + cur, 0);
 
 console.log(`Result of normal JS reduce - ${result2}`);
