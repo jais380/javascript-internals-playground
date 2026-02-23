346c77e557edd1bb2531f26d7f6942bed2a1f8de
JUDE <judeani60@gmail.com>
2026-02-23 22:39:28 +0100

    feat: create judeReduce()

--- 04_customReduce.js
+++ 04_customReduce.js
@@ -4,4 +4,20 @@ let arr = [4, 5, 6, 8, 9];
 
 let result1 = arr.reduce((acc, cur) => acc + cur, 0)
 
-console.log(result1);
+console.log(`Result of normal JS reduce - ${result1}`);
+
+Array.prototype.judeReduce = function(myCallback, initialValue) {
+
+    let accumulatedValue = initialValue;
+
+    for(let i = 0; i < this.length; i++) {
+        accumulatedValue = myCallback(accumulatedValue, this[i]);
+    }
+
+    return accumulatedValue;
+}
+
+
+let result2 = arr.judeReduce((acc, cur) => acc + cur, 0);
+
+console.log(`Result of normal JS reduce - ${result2}`);
