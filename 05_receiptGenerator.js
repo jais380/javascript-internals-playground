eb304a58140e5b3289db033374d63e6289d3aa8d
JUDE <judeani60@gmail.com>
2026-02-24 21:55:44 +0100

    feat: create commerce receipt generator

---
+++ 05_receiptGenerator.js
@@ -0,0 +1,21 @@
+const products = [
+    {name: 'Jordan sneakers', price: 100, instock: true},
+    {name: 'Rollerskates', price: 135, instock: false},
+    {name: 'Jungle boots', price: 90, instock: false},
+    {name: 'Comabat boots', price: 85, instock: true},
+    {name: 'Slippers', price: 12, instock: false},
+    {name: 'Sandals', price: 45, instock: true},
+    {name: 'Ice skates', price: 154, instock: true}
+]
+
+const cart = products.filter((item) => item.instock === true);
+
+console.log(cart);
+
+const taxedProducts = cart.map((item) => item.price + (item.price * 0.1));
+
+console.log(taxedProducts);
+
+const finalPrice = taxedProducts.reduce((acc, cur) => acc + cur, 0);
+
+console.log(finalPrice);
