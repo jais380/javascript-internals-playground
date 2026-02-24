206eb36ef53dfe9ff865ae10e174d35687013433
JUDE <judeani60@gmail.com>
2026-02-24 22:05:40 +0100

    chore: add comments

--- 05_receiptGenerator.js
+++ 05_receiptGenerator.js
@@ -8,14 +8,17 @@ const products = [
     {name: 'Ice skates', price: 154, instock: true}
 ]
 
+// Gets all the products that are still in stock.
 const cart = products.filter((item) => item.instock === true);
 
 console.log(cart);
 
+// Adds the 10% VAT to each product price.
 const taxedProducts = cart.map((item) => item.price + (item.price * 0.1));
 
 console.log(`Prices of cart items with 10% value added tax -> ${taxedProducts}`);
 
+// Calculate and return the final price of the products to be bought.
 const finalPrice = taxedProducts.reduce((acc, cur) => acc + cur, 0);
 
 console.log(`Total price of items -> ${finalPrice}`);
