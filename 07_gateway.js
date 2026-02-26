28b23d3411c8a21d4788b341a2a0037104bc43af
JUDE <judeani60@gmail.com>
2026-02-26 23:17:50 +0100

    feat: creat middleware check

--- 07_gateway.js
+++ 07_gateway.js
@@ -3,7 +3,14 @@ const users = [
     {name: 'Simon', age: '9', hasSubscription: false, role: 'guest'},
     {name: 'John', age: '44', hasSubscription: true, role: 'guest'},
     {name: 'Jimmy', age: '16', hasSubscription: false, role: 'admin'},
-    {name: 'Roland', age: '14', hasSubscription: false, role: 'guest'},
+    {name: 'Roland', age: '14', hasSubscription: true, role: 'guest'},
     {name: 'Cynthia', age: '33', hasSubscription: false, role: 'guest'},
     {name: 'Drago', age: '25', hasSubscription: true, role: 'guest'}
 ]
+
+const statusMessages = users.map((user) => user.role === 'admin' ? "Access Granted: Welcome Boss" :
+
+            user.hasSubscription && user.age > 18 ? "Access Granted: Enjoy the Pro features" : "Access Denied: Please upgrade or check age");
+
+
+console.log(statusMessages);
