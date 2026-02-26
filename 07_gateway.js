1cfb66530275267600020ddbfa2bc8522227634b
JUDE <judeani60@gmail.com>
2026-02-26 23:08:18 +0100

    first commit for gateway

---
+++ 07_gateway.js
@@ -0,0 +1,9 @@
+const users = [
+    {name: 'Jude', age: '88', hasSubscription: true, role: 'admin'},
+    {name: 'Simon', age: '9', hasSubscription: false, role: 'guest'},
+    {name: 'John', age: '44', hasSubscription: true, role: 'guest'},
+    {name: 'Jimmy', age: '16', hasSubscription: false, role: 'admin'},
+    {name: 'Roland', age: '14', hasSubscription: false, role: 'guest'},
+    {name: 'Cynthia', age: '33', hasSubscription: false, role: 'guest'},
+    {name: 'Drago', age: '25', hasSubscription: true, role: 'guest'}
+]
