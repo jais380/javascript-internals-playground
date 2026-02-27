e371a2bc198248841b7b3cafbd94b625a7337638
JUDE <judeani60@gmail.com>
2026-02-27 23:21:43 +0100

    refactor: test judeMap usecase

--- 02_customMap.js
+++ 02_customMap.js
@@ -34,3 +34,24 @@ res.judeMap((num) => num * 2);
 
 // Prints only newArr
 console.log(res.newArr);
+
+
+// List of Users data
+const users = [
+    {name: 'Jude', age: '88', hasSubscription: true, role: 'admin'},
+    {name: 'Simon', age: '9', hasSubscription: false, role: 'guest'},
+    {name: 'John', age: '44', hasSubscription: true, role: 'guest'},
+    {name: 'Jimmy', age: '16', hasSubscription: false, role: 'admin'},
+    {name: 'Roland', age: '14', hasSubscription: true, role: 'guest'},
+    {name: 'Cynthia', age: '33', hasSubscription: false, role: 'guest'},
+    {name: 'Drago', age: '25', hasSubscription: true, role: 'guest'}
+]
+
+
+// .map() used to perform middleware checks to display status message for different users
+const statusMessages = users.map((user) => user.role === 'admin' ? "Access Granted: Welcome Boss" :
+
+            user.hasSubscription && user.age > 18 ? "Access Granted: Enjoy the Pro features" : "Access Denied: Please upgrade or check age");
+
+
+console.log(statusMessages);
--- 03_customFilter.js
+++ 03_customFilter.js
@@ -35,24 +35,3 @@ Array.prototype.judeFilter = function(myCallback) {
 const result2 = arr.judeFilter((num) => num % 2 === 0 && num !== 0);
 
 console.log(`Result of my custom filter - ${result2}`);
-
-
-// List of Users data
-const users = [
-    {name: 'Jude', age: '88', hasSubscription: true, role: 'admin'},
-    {name: 'Simon', age: '9', hasSubscription: false, role: 'guest'},
-    {name: 'John', age: '44', hasSubscription: true, role: 'guest'},
-    {name: 'Jimmy', age: '16', hasSubscription: false, role: 'admin'},
-    {name: 'Roland', age: '14', hasSubscription: true, role: 'guest'},
-    {name: 'Cynthia', age: '33', hasSubscription: false, role: 'guest'},
-    {name: 'Drago', age: '25', hasSubscription: true, role: 'guest'}
-]
-
-
-// .map() used to perform middleware checks to display status message for different users
-const statusMessages = users.map((user) => user.role === 'admin' ? "Access Granted: Welcome Boss" :
-
-            user.hasSubscription && user.age > 18 ? "Access Granted: Enjoy the Pro features" : "Access Denied: Please upgrade or check age");
-
-
-console.log(statusMessages);
