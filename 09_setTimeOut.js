2b98cd5acb934e645acd3f7994070c2be78b975c
JUDE <judeani60@gmail.com>
2026-03-01 22:21:54 +0100

    chore: add comment

--- 09_setTimeOut.js
+++ 09_setTimeOut.js
@@ -1,22 +1,30 @@
+// UNDERSTANDING setTimeout
+
+// SIXTH/LAST TO PRINT
 setTimeout(() => {
     console.log('Javascript');
 }, 1000)
 
 
+// FIRST TO PRINT
 console.log("Jude");
 
 
+// FOURTH TO PRINT
 setTimeout(() => {
     console.log('Async');
 }, 0)
 
 
+// SECOND TO PRINT
 console.log(60);
 
 
+// FIFTH TO PRINT
 setTimeout(() => {
     console.log('in');
 }, 0)
 
 
+// THIRD TO PRINT
 console.log(105);
