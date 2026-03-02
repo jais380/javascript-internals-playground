7a9267cf27ea8ae4eb4715cab5363b7fcde6021a
JUDE <judeani60@gmail.com>
2026-03-02 23:41:17 +0100

    chore: add comment

--- 10_ghostLogger.js
+++ 10_ghostLogger.js
@@ -1,9 +1,17 @@
+//List of users
 users = ['Jude', 'Judith', 'Jimmy', 'Roland', 'Ayo'];
 
+
+//Loop through each list and its index
 users.forEach((user, index) => {
+
+    //Apply setTimeout for delayed response
     setTimeout(() => {
+
+        // Ternary Operator to print necessary output
         let userLog = user === 'Jude' ? `Scanning Admin ${user}...` : `Scanning Guest ${user}...`;
 
+        // Print the user log, watch the magic
         console.log(userLog);
 
     }, index*2000)
