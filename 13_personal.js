dc067c977f4fcb85e1f341e8412c18087c8b13e3
JUDE <judeani60@gmail.com>
2026-03-05 22:11:04 +0100

    chore: add comment

--- 13_personal.js
+++ 13_personal.js
@@ -1,12 +1,19 @@
+// Working with destructuring and spread operator
+
+// Object
 const value1 = {a: "Jude", b: "Tired"};
 
+// Object destructured
 const {a: name, b: status} = value1;
 
+// Print message
 console.log(`${name} is very ${status}`);
 
-
+// List
 const arr1 = ["sleepy", "hungry"];
 
+// Second List with spread operator joining the first list to it
 const arr2 = [...arr1, "broke", "single"];
 
+// Print message
 console.log(`${name} is also very ${arr2}`);
