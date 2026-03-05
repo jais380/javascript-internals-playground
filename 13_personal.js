a43a5691f8409910a40fea2981683ac736645def
JUDE <judeani60@gmail.com>
2026-03-05 22:11:48 +0100

    chore: add fullstop to comment

--- 13_personal.js
+++ 13_personal.js
@@ -1,19 +1,19 @@
-// Working with destructuring and spread operator
+// Working with destructuring and spread operator.
 
-// Object
+// Object.
 const value1 = {a: "Jude", b: "Tired"};
 
-// Object destructured
+// Object destructured.
 const {a: name, b: status} = value1;
 
-// Print message
+// Print message.
 console.log(`${name} is very ${status}`);
 
-// List
+// List.
 const arr1 = ["sleepy", "hungry"];
 
-// Second List with spread operator joining the first list to it
+// Second List with spread operator joining the first list to it.
 const arr2 = [...arr1, "broke", "single"];
 
-// Print message
+// Print message.
 console.log(`${name} is also very ${arr2}`);
