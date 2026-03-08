16ef7bed4b8f2fb813b9d6564bb08e7e5182758d
JUDE <judeani60@gmail.com>
2026-03-08 23:01:36 +0100

    chore: update code

--- 15_async.js
+++ 15_async.js
@@ -1,30 +1,33 @@
 // Set Timeout use case
+setTimeout(() => {console.log("I am Atomic!!");}, 0);
 
-    setTimeout(() => {console.log("I am Atomic!!");}, 0);
+// Promises use case
+async function getUsersFromFacebook() {
+    const prom = new Promise((resolve, reject) => {
+        let users = [{name:"Jude", age:0}, {name:"Johnny", age:99}];
 
+        if(users.length>0) {
+            resolve(data = {response:users, message:"Success!!!"});
+        }else {
+            reject(errorMessage = {Error:"Better Luck next time :( "});
+        }
+    });
+    return prom;
+}
 
-    async function getUsersFromFacebook() {
-        const prom = new Promise((resolve, reject) => {
-            let users = [{name:"Jude", age:0}, {name:"Johnny", age:99}];
+// Handling data returned by promise
+getUsersFromFacebook().then((data) => {
+    console.log(data);
 
-            if(users.length>0) {
-                resolve(data = {response:users, message:"Success!!!"});
-            }else {
-                reject(errorMessage = {Error:"Better Luck next time :( "});
-            }
-        });
-        return prom;
-    }
+    return data.response[1];
+})
+.then((res) => {
+    console.log(res.name);
+})
+.catch((err) => {
+    console.log(err);
+})
 
+// Normal print
 
-    getUsersFromFacebook().then((data) => {
-        console.log(data);
-
-        return data.response[1];
-    })
-    .then((res) => {
-        console.log(res.name);
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+console.log(`I am the first!!`);
