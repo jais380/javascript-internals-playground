b33e423d256706e2fd72a1b3ef7bcde1fe0a9a47
JUDE <judeani60@gmail.com>
2026-03-08 22:56:35 +0100

    chore: correct linting

--- 15_async.js
+++ 15_async.js
@@ -1,37 +1,30 @@
-// PROMISES
-            setTimeout(() => {
-                console.log("I am Atomic!!");
-            }, 5000);
+// Set Timeout use case
+
+    setTimeout(() => {console.log("I am Atomic!!");}, 0);
 
 
-            async function getUsersFromFacebook() {
-                const prom = new Promise((resolve, reject) => {
-                    let users = [{name:"Jude", age:0}, {name:"Johnny", age:99}];
+    async function getUsersFromFacebook() {
+        const prom = new Promise((resolve, reject) => {
+            let users = [{name:"Jude", age:0}, {name:"Johnny", age:99}];
 
-                    if(users.length>0) {
-                        resolve(data = {response:users, message:"Success!!!"});
-                    }
-                    else {
-                        reject(errorMessage = {Error:"Better Luck next time :( "});
-                    }
-                });
-
-                return prom;
+            if(users.length>0) {
+                resolve(data = {response:users, message:"Success!!!"});
+            }else {
+                reject(errorMessage = {Error:"Better Luck next time :( "});
             }
+        });
+        return prom;
+    }
 
 
-            getUsersFromFacebook()
-            .then((data) => {
+    getUsersFromFacebook().then((data) => {
+        console.log(data);
 
-                console.log(data);
-
-                return data.response[1];
-            })
-            .then((res) => {
-
-                // only one output because the return ended the forloop
-                console.log(res.name);
-            })
-            .catch((err) => {
-                console.log(err);
-            })
+        return data.response[1];
+    })
+    .then((res) => {
+        console.log(res.name);
+    })
+    .catch((err) => {
+        console.log(err);
+    })
