1b5d75377013892bf722b5359bf988e015faecf6
JUDE <judeani60@gmail.com>
2026-03-11 20:26:18 +0100

    New commit

--- express/App2.js
+++ express/App2.js
@@ -7,30 +7,29 @@ app.use(express.json());
 const port = 3000;
 const students = [];
 
-
 app.post("/add", (req, res) => {
 
     console.log(req.body);
 
+    // validation check
     if(!req.body.id || !req.body.full_name || !req.body.email) {
 
         res.status(400).send('All fields are required');
 
     } else {
-        const {id, full_name, email } = req.body;
+        const {id, full_name, email } = req.body; // Ensure only these fields are stored
 
         const newStudent = {id, full_name, email};
 
-        students.push(newStudent);
+        students.push(newStudent); // Store data
 
         res.status(201).send(`${newStudent.full_name} added successfully`);
     }
 });
 
-
 app.get("/list", (req, res) => {
 
-    res.send(students);
+    res.send(students); // get data
 })
 
 app.listen(port, () => {
