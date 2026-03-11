fc9bf49e68f92858197cda3c40a269214b50fab0
JUDE <judeani60@gmail.com>
2026-03-11 20:18:52 +0100

    feat: add post endpoint

---
+++ express/App2.js
@@ -0,0 +1,31 @@
+import express from 'express';
+
+const app = express();
+
+app.use(express.json());
+
+const port = 3000;
+const students = [];
+
+
+app.post("/add", (req, res) => {
+
+    console.log(req.body);
+
+    if(!req.body.id || !req.body.full_name || !req.body.email) {
+
+        res.status(400).send('All fields are required');
+
+    } else {
+        const {id, full_name, email } = req.body;
+
+        const newStudent = {id, full_name, email};
+
+        students.push(newStudent);
+    }
+});
+
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
