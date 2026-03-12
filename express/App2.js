395623d426adcc20360267f4c6c2d1ddb1bf76e7
JUDE <judeani60@gmail.com>
2026-03-12 21:38:03 +0100

    refactor: update post and get endpoint

--- express/App2.js
+++ express/App2.js
@@ -6,22 +6,23 @@ app.use(express.json()); // In order for express to parse the JSON data
 
 const port = 3000;
 const students = [];
+let count = 1;
 
 app.post("/add", (req, res) => {
 
     console.log(req.body);
 
     // validation check
-    if(!req.body.id || !req.body.full_name || !req.body.email) {
+    if(!req.body.full_name || !req.body.email) {
 
         res.status(400).send('All fields are required');
 
     } else {
-        const {id, full_name, email } = req.body; // Ensure only these fields are stored
+        const {full_name, email } = req.body; // Ensure only these fields are stored
 
-        const newStudent = {id, full_name, email};
+        const newStudent = {full_name, email};
 
-        students.push(newStudent); // Store data
+        students.push({id: count++, ...newStudent}); // Store data
 
         res.status(201).send(`${newStudent.full_name} added successfully`);
     }
@@ -29,8 +30,9 @@ app.post("/add", (req, res) => {
 
 app.get("/list", (req, res) => {
 
-    res.send(students); // get data
-})
+    res.status(200).send(students); // get data
+});
+
 
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
