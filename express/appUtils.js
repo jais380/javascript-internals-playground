dfdfb5bbc2d7d7ec96082225f2b9e1fae0e05a0c
JUDE <judeani60@gmail.com>
2026-03-15 23:29:02 +0100

    chore: add comment

--- express/appUtils.js
+++ express/appUtils.js
@@ -2,13 +2,13 @@ const products = [];
 let count = 1;
 
 export const addproduct = (req, res) => {
-    const {productName, price, quantity, inStock} = req.body;
+    const {productName, price, quantity, inStock} = req.body; // select only relevant fields
 
-    const data = {productName, price, quantity, inStock};
+    const data = {productName, price, quantity, inStock}; // assing the values to data variable
 
-    const product = {id: count++, ...data};
+    const product = {id: count++, ...data}; // apply dynamic id generation
 
-    products.push(product);
+    products.push(product); // add to in memory storage
 
     res.status(201).json({product})
 }
