35245f62649a060600c64f2ae62841a23e660f37
JUDE <judeani60@gmail.com>
2026-03-15 23:27:23 +0100

    chore: add comment

--- express/App3.js
+++ express/App3.js
@@ -2,7 +2,7 @@ import express from 'express'
 import { addproduct, getproducts } from './appUtils.js';
 
 const app = express();
-const port = 3001;
+const port = 3001; //port
 
 app.use(express.json()) //middleware
 
